d951fa32338b0ec9b1eb05f0054e3dc24170542e
Autophagy <mail@deadcells.org>
2016-04-05 21:56:53 +0200

    Added error handling

--- app/museumzeit.py
+++ app/museumzeit.py
@@ -8,5 +8,14 @@ Bootstrap(app)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('error.html', errorCode='404', errorDesc='page not found'), 404
+
+@app.errorhandler(500)
+def internalServerError(error):
+    return render_template('error.html', errorCode='500', errorDesc='internal server error'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
