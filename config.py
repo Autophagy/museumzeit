f79243d7ea86498a36c0153efdd3ffacb696aaf6
Autophagy <mail@deadcells.org>
2016-04-13 21:04:30 +0200

    Created config script

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+base = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base, 'data.sqlite'
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
