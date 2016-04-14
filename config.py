9399a5f5e37909230b3899ac2b47a1e10d7740d7
Autophagy <mail@deadcells.org>
2016-04-14 15:56:45 +0200

    Added track modification supression

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ base = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
     def init_app(app):
