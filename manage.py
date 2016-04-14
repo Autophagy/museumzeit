d5cc72effa09f4b3a405ec21b065629ad4ee8505
Autophagy <mail@deadcells.org>
2016-04-14 15:57:55 +0200

    Updated management script to import new models

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import Country, City
+from app.models import Country, City, Type, Museum, Period
 from flask.ext.script import Manager, Shell
 
 app = create_app('development')
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, db=db, Country=Country, City=City)
+    return dict(app=app, db=db, Country=Country, City=City, Type=Type, Museum=Museum, Period=Period)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
