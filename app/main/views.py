dc3de60655f85e7b9ce0829b8d8aabed599d53ed
Autophagy <mail@deadcells.org>
2016-04-21 16:22:07 +0200

    If no city is found, a 404 is thrown

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for, current_app
+from flask import render_template, session, redirect, url_for, current_app, abort
 from .. import db
 from ..models import Country, City
 from . import main
@@ -11,4 +11,9 @@ def index():
 @main.route('/<cityname>')
 def city(cityname):
     city = City.query.filter(City.name.ilike(cityname)).first()
+
+    # If no city is found, throw a 404 error
+    if city == None:
+        abort(404)
+
     return render_template('city.html', city=city)
