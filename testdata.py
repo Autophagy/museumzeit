68bef465baad7c347f3de5f6e82f4b3ad8bf266b
Autophagy <mail@deadcells.org>
2016-04-28 23:57:15 +0200

    Added test periods for london museums

--- testdata.py
+++ testdata.py
@@ -38,6 +38,10 @@ museums = [britishMuseum, scienceMuseum, nationalGallery, naturalHistoryMuseum, 
 # Periods
 
 bmPeriod = Period(openTime=datetime.time(10,0,0), closedTime=datetime.time(17, 30, 0), free=True, museum=britishMuseum)
+smPeriod = Period(openTime=datetime.time(8,0,0), closedTime=datetime.time(20, 0, 0), free=True, museum=scienceMuseum)
+ngPeriod = Period(openTime=datetime.time(9,30,0), closedTime=datetime.time(14, 30, 0), free=True, museum=nationalGallery)
+nhmPeriod = Period(openTime=datetime.time(11,0,0), closedTime=datetime.time(19, 0, 0), free=True, museum=naturalHistoryMuseum)
+
 pmPeriod = Period(openTime=datetime.time(10,0,0), closedTime=datetime.time(18, 30, 0), free=False, museum=pergamonMuseum)
 
 periods = [bmPeriod, pmPeriod]
